fc8ed9baeba18d80f56b2eac0454f0e5f19b69b8
Miguel Braga <101603810+Miguelhttp@users.noreply.github.com>
2023-10-27 10:16:27 -0300

    feat: add icons techs in the page About

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import styles from './About.module.css'
+import avatar from './image/avatar-02.png'
+import html from './image/icon-html.svg'
+import css from './image/icon-css.svg'
+import js from './image/icon-js.svg'
+import react from './image/icon-react.svg'
 
 const About = () => {
   return (
     <React.Fragment>
-      <h2>About</h2>
+      <section className={styles.about}>
+        <div className={styles.bio}>
+          <img className={styles.avatar} src={avatar} alt="Avatar" />
+          <div className={styles.text_about}>
+            <h2>Sobre</h2>
+
+            <p>
+              Sou <span>Miguel Braga</span> <br />
+              <strong>Front End Developer</strong>
+            </p>
+
+            <p>Estudante de Programação</p>
+          </div>
+        </div>
+
+        <div className={styles.techs}>
+          <h3>Techs</h3>
+          <div className={styles.icons_techs}>
+            <img src={html} alt="Ícone do HTMl" />
+            <img src={css} alt="Ícone do CSS" />
+            <img src={js} alt="Ícone do JavaScript" />
+            <img src={react} alt="Ícone do React" />
+          </div>
+        </div>
+      </section>
     </React.Fragment>
   )
 }
