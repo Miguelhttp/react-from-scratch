696526a894d5af42db2dd7b0982d28bdb40fd2b2
Miguel Braga <101603810+Miguelhttp@users.noreply.github.com>
2023-10-27 08:15:41 -0300

    chore: add container

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
-
-import './Home.module.css'
+import styles from './Home.module.css'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import { Link } from 'react-router-dom'
+import Container from '../../components/Container'
 const Home = () => {
   return (
     <React.Fragment>
       <Header />
-      {/* Conteúdo */}
-      <section className="container">
-        <div className="apresentacao">
-          <p>
-            Hello, I am <br />
-            <span>Miguel Braga</span> <br />
-            Dev Front End
-          </p>
+      <Container>
+        <section className={styles.home}>
+          <div className={styles.apresentacao}>
+            <p>
+              Hello, I am <br />
+              <span>Miguel Braga</span> <br />
+              Dev Front End
+            </p>
 
-          <Link to="/about" className="btn btn-purple">
-            Read More
-          </Link>
-        </div>
+            <Link to="/about" className={`${styles.btn} ${styles.btn_purple}`}>
+              Read More
+            </Link>
+          </div>
 
-        <figure>
-          <img
-            className="img-home"
-            src="/developer-front.svg"
-            alt="Imagem de Home"
-          />
-        </figure>
-      </section>
+          <figure>
+            <img
+              className={styles.img_home}
+              src="/developer-front.svg"
+              alt="Imagem de Home"
+            />
+          </figure>
+        </section>
+      </Container>
       <Footer />
     </React.Fragment>
   )
