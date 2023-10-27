bef1283052118794592a6028493882896d853ada
Miguel Braga <101603810+Miguelhttp@users.noreply.github.com>
2023-10-27 09:39:22 -0300

    feat: add PageBase

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,27 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PageBase from '../pages/PageBase'
+import PageError from '../pages/PageError'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Project from '../pages/Project'
 import Contact from '../pages/Contact'
-import PageError from '../pages/PageError'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import Container from '../components/Container'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<PageBase />}>
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/project" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<PageError />} />
-        </Routes>
-      </Container>
-      <Footer />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
