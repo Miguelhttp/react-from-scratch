61bb085411cdd411c05339961326d136ae387393
Miguel Braga <101603810+Miguelhttp@users.noreply.github.com>
2023-10-27 08:32:29 -0300

    feat: add styles

--- src/pages/PageError/index.jsx
+++ src/pages/PageError/index.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Header from '../../components/Header'
 import Container from '../../components/Container'
 import Footer from '../../components/Footer'
+import styles from './PageError.module.css'
 
 const PageError = () => {
   return (
     <React.Fragment>
       <Header />
       <Container>
-        <h2>Página não encontrada</h2>
-        <div>
-          <span>Erro 404</span> <br />
-          <strong>Página não encontrada</strong>
+        <h2 className={styles.subtitle}>Página não encontrada</h2>
+        <div className={styles.texts}>
+          <span className={styles.text_big}>Erro 404</span> <br />
+          <strong className={styles.text_purple}>Página não encontrada</strong>
         </div>
       </Container>
       <Footer />
