94aa396a4da5ba404df8df289c059420fe346eb2
Miguel Braga <101603810+Miguelhttp@users.noreply.github.com>
2023-10-27 15:02:17 -0300

    chore: adjusting mobile responsiveness

--- src/pages/PageError/index.jsx
+++ src/pages/PageError/index.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import styles from './PageError.module.css'
 
 const PageError = () => {
   return (
-    <React.Fragment>
+    <section className={styles.page_error}>
       <h2 className={styles.subtitle}>Página não encontrada</h2>
       <div className={styles.texts}>
         <span className={styles.text_big}>Erro 404</span> <br />
         <strong className={styles.text_purple}>Página não encontrada</strong>
       </div>
-    </React.Fragment>
+    </section>
   )
 }
 
